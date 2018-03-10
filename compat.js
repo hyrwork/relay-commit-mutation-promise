b82337506842677d41d679774371b2773336d1fe
LuisRizo <personal.luisrizo@gmail.com>
2018-03-10 13:28:58 -0500

    Created requestSubscriptionPromise

--- compat.js
+++ compat.js
@@ -24,4 +24,28 @@ function commitMutationPromise(environment, config) {
     });
 }
 
+export const requestSubscriptionPromise = function (environment, config) {
+    return new Promise(function (resolve, reject) {
+        Relay.requestSubscription(
+            environment,
+            Object.assign(
+                {},
+                config,
+                {
+                    onCompleted: function (payload, errors) {
+                        if (errors) {
+                            reject(errors[0]);
+                            return;
+                        }
+                        resolve(payload);
+                    },
+                    onError: function (error) {
+                        reject(error);
+                    }
+                }
+            )
+        );
+    });
+}
+
 module.exports = commitMutationPromise;
--- index.js
+++ index.js
@@ -24,4 +24,28 @@ function commitMutationPromise(environment, config) {
     });
 }
 
-module.exports = commitMutationPromise;
+export const requestSubscriptionPromise = function (environment, config) {
+    return new Promise(function (resolve, reject) {
+        Relay.requestSubscription(
+            environment,
+            Object.assign(
+                {},
+                config,
+                {
+                    onCompleted: function (payload, errors) {
+                        if (errors) {
+                            reject(errors[0]);
+                            return;
+                        }
+                        resolve(payload);
+                    },
+                    onError: function (error) {
+                        reject(error);
+                    }
+                }
+            )
+        );
+    });
+}
+
+export default commitMutationPromise
